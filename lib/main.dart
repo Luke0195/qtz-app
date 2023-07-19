d507b4b6d65d5d8fe66c22c94c92e78f8a67dfba
Lucas Santos <lucas201273@gmail.com>
2023-07-19 20:02:15 -0300

    initial form

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,52 @@
+// Scafold é o widget que trás algumas coisa já prontas para gente
+// o body não consegue  receber mais um componente
+
 import 'package:flutter/material.dart';
 
-main() {}
+main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('My First App'),
-    );
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+                title: Container(
+              alignment: Alignment.center,
+              child: const Text('Quiz Project!', textAlign: TextAlign.center),
+            )),
+            body: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  alignment: Alignment.center,
+                  child: const Column(children: [
+                    Text('Tela de Login',
+                        style: TextStyle(
+                            color: Colors.black38,
+                            fontSize: 23,
+                            fontFamily: AutofillHints.creditCardName)),
+                    Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter with your name',
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Colors.grey,
+                              )),
+                          enabled: true,
+                        )
+                      ],
+                    )
+                  ]),
+                )
+              ],
+            )));
   }
 }
