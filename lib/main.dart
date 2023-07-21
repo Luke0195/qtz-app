d4ebe991fd8760523f47b7e5be28cd2e0d95af2e
Lucas Santos <lucas201273@gmail.com>
2023-07-20 22:19:28 -0300

    feat: add questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,67 @@
-// Scafold é o widget que trás algumas coisa já prontas para gente
-// o body não consegue  receber mais um componente
-
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(const MyApp());
-}
+main() {}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'Qual é a sua Linguagem favorita',
+      'Qual framework você mais odeia',
+      'Qual a sua visão para o futuro'
+    ];
+
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-                title: Container(
-              alignment: Alignment.center,
-              child: const Text('Quiz Project!', textAlign: TextAlign.center),
-            )),
-            body: Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  alignment: Alignment.center,
-                  child: const Column(children: [
-                    Text('Tela de Login',
-                        style: TextStyle(
-                            color: Colors.black38,
-                            fontSize: 23,
-                            fontFamily: AutofillHints.creditCardName)),
-                    Column(
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Enter with your name',
-                              prefixIcon: Icon(
-                                Icons.search,
-                                color: Colors.grey,
-                              )),
-                          enabled: true,
-                        )
-                      ],
-                    )
-                  ]),
-                )
-              ],
-            )));
+      appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: Text('Quiz Project')),
+              Icon(Icons.quiz)
+            ],
+          )),
+      body: Column(children: [
+        Text(questions[0]),
+        ElevatedButton(
+          onPressed: null,
+          style: TextButton.styleFrom(
+              fixedSize: const Size(240, 45),
+              backgroundColor: Colors.purple,
+              disabledBackgroundColor: Colors.grey),
+          child: const Text('Reposta  1',
+              style: TextStyle(
+                color: Colors.white,
+              )),
+        ),
+        ElevatedButton(
+          onPressed: null,
+          style: TextButton.styleFrom(
+              fixedSize: const Size(240, 45),
+              backgroundColor: Colors.purple,
+              disabledBackgroundColor: Colors.grey),
+          child: const Text('Reposta  2',
+              style: TextStyle(
+                color: Colors.white,
+              )),
+        ),
+        ElevatedButton(
+          onPressed: null,
+          style: TextButton.styleFrom(
+              fixedSize: const Size(240, 45),
+              backgroundColor: Colors.purple,
+              disabledBackgroundColor: Colors.grey),
+          child: const Text('Reposta  2',
+              style: TextStyle(
+                color: Colors.white,
+              )),
+        )
+      ]),
+    ));
   }
 }
